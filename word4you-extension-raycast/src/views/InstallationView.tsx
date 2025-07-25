449f286742f30e72722e9d7070d1ffd1e6eda886
gnehz972 <zoz20042008@gmail.com>
2025-07-25 14:38:28 +0800

    Remove action

--- word4you-extension-raycast/src/views/InstallationView.tsx
+++ word4you-extension-raycast/src/views/InstallationView.tsx
@@ -9,11 +9,6 @@ export function InstallationView() {
         description="Download and setup the CLI, then configure the full path in the extension preference"
         actions={
           <ActionPanel>
-            <Action
-              title="Download Word4you Cli"
-              icon={Icon.Download}
-              onAction={() => open("https://github.com/gnehz972/word4you/releases")}
-            />
             <Action title="Open Extension Preferences" icon={Icon.Gear} onAction={openExtensionPreferences} />
           </ActionPanel>
         }
